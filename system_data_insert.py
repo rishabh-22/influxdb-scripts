343f461954945850b9466679fa914319febd0293
rishabh-22 <rishabh.bh22@gmail.com>
2019-11-14 11:12:06 +0530

    network data improvements

--- system_data_insert.py
+++ system_data_insert.py
@@ -8,20 +8,22 @@ client.create_database('system')
 measurement_name = 'system_data'
 data_end_time = int(time.time() * 1000)
 data = []
-cpu_p, mem_p, disk_read, disk_write, net_sent, net_recv, temp, boot_time = \
-    0, 0, 0, 0, 0, 0, 0, 0
+cpu_p, mem_p, disk_read, disk_write, net_sent_now, net_recv_now, temp, \
+    boot_time, net_sent_prev, net_recv_prev = \
+    0, 0, 0, 0, 0, 0, 0, 0, \
+    psutil.net_io_counters().bytes_sent, psutil.net_io_counters().bytes_recv
 
 
 def get_system_data():
-    global cpu_p, mem_p, disk_write, disk_read, net_recv, net_sent, temp, \
-        boot_time, data_end_time
+    global cpu_p, mem_p, disk_write, disk_read, net_recv_now, net_sent_now,\
+        temp, boot_time, data_end_time
     data_end_time = int(time.time() * 1000)
     cpu_p = psutil.cpu_percent()
     mem_p = psutil.virtual_memory().percent
     disk_read = psutil.disk_io_counters().read_count
     disk_write = psutil.disk_io_counters().write_count
-    net_sent = psutil.net_io_counters().bytes_sent
-    net_recv = psutil.net_io_counters().bytes_recv
+    net_sent_now = psutil.net_io_counters().bytes_sent
+    net_recv_now = psutil.net_io_counters().bytes_recv
     temp = psutil.sensors_temperatures()['acpitz'][0].current
     boot_time = psutil.boot_time()
 
@@ -36,8 +38,8 @@ def get_system_data():
                 "memory_percent": mem_p,
                 "disk_read": disk_read,
                 "disk_write": disk_write,
-                "net_sent": net_sent,
-                "net_received": net_recv,
+                "net_sent": net_sent_now-net_sent_prev,
+                "net_received": net_recv_now-net_recv_prev,
                 "temperature": temp,
             },
             "time": data_end_time
@@ -48,10 +50,13 @@ def get_system_data():
                         protocol='json')
 
 
-def run(interval=15):  # interval in seconds
+def run(interval=5):  # interval in seconds
+    global net_recv_prev, net_sent_prev
     while 1:
         try:
             get_system_data()
+            net_sent_prev = psutil.net_io_counters().bytes_sent
+            net_recv_prev = psutil.net_io_counters().bytes_recv
             time.sleep(interval)
         except KeyboardInterrupt:
             quit()
